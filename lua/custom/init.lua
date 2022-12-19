1a2579951f4b8ad71b68a44fb3326691336bfe2b
xqft <estefano.bargas@fing.edu.uy>
2022-12-19 20:56:09 -0300

    Added user settings

---
+++ lua/custom/init.lua
@@ -0,0 +1,15 @@
+local opt = vim.op
+
+-- NVIM OPTS --
+
+-- Line number
+opt.relativenumber = true
+
+-- Tabs
+opt.tabstop     = 2
+opt.shiftwidth  = 0
+opt.expandtab   = true
+
+-- Searching
+opt.ignorecase  = true
+opt.smartcase   = true
